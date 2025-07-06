23f9ac0f81ccbd40504732d35ef940d3702ec4e4
jramsgz <47223013+jramsgz@users.noreply.github.com>
2025-07-06 16:32:27 +0200

    Enhance climate project by adding support for additional relays and improving temperature validation checks

--- projects/climate/code/main.py
+++ projects/climate/code/main.py
@@ -30,7 +30,11 @@ ds_exterior_pin = 20 # GPIO pin for exterior temperature data
 relay_caldera_pin = 13 # GPIO pin for caldera relay
 relay_acs_pin = 9 # GPIO pin for ACS relay
 relay_primerod_pin = 10 # GPIO pin for primerod relay
-relay_garaje_pin = 15 # GPIO pin for garaje relay
+relay_garaje_pin = 14 # GPIO pin for garaje relay
+relay_gen1_pin = 15 # GPIO pin for relay
+relay_gen2_pin = 12 # GPIO pin for relay
+relay_gen3_pin = 11 # GPIO pin for relay
+relay_gen4_pin = 8 # GPIO pin for relay
 pir_pin = 18 # GPIO pin for the PIR sensor
 
 ds_caldera_sensor = None  # caldera temperature sensor object (Dallas)
@@ -41,6 +45,10 @@ relay_caldera = None # relay for caldera
 relay_acs = None # relay for acs
 relay_primerod = None # relay for primerod
 relay_garaje = None # relay for garaje
+relay_gen1 = None # relay
+relay_gen2 = None # relay
+relay_gen3 = None # relay
+relay_gen4 = None # relay
 temperatura_caldera = None # temperatura caldera
 temperatura_deposito = None # temperatura deposito
 temperatura_casa = None # temperatura casa
@@ -80,14 +88,22 @@ def getTemperature():
         print("Error getting exterior temperature: " + str(e))
     
     # Check temperature values are valid
-    if temperatura_caldera < 0 or temperatura_caldera > 84:
-        temperatura_caldera = None
-    if temperatura_deposito < 0 or temperatura_deposito > 84:
-        temperatura_deposito = None
-    if temperatura_casa < 0 or temperatura_casa > 50:
-        temperatura_casa = None
-    if temperatura_exterior < -20 or temperatura_exterior > 60:
-        temperatura_exterior = None
+    if temperatura_caldera is not None:
+        if temperatura_caldera == 85.0:
+            temperatura_caldera = None
+        if temperatura_caldera < 0 or temperatura_caldera > 100:
+            temperatura_caldera = None
+    if temperatura_deposito is not None:
+        if temperatura_deposito == 85.0:
+            temperatura_deposito = None
+        if temperatura_deposito < 0 or temperatura_deposito > 90:
+            temperatura_deposito = None
+    if temperatura_casa is not None:
+        if temperatura_casa < -10 or temperatura_casa > 60:
+            temperatura_casa = None
+    if temperatura_exterior is not None:
+        if temperatura_exterior < -30 or temperatura_exterior > 70:
+            temperatura_exterior = None
 
 def msg_received(topic, msg, retained, duplicate):
     if topic == "system/status":
@@ -112,6 +128,26 @@ def msg_received(topic, msg, retained, duplicate):
             relay_garaje.value(0)
         elif msg == b"False":
             relay_garaje.value(1)
+    elif topic == "switch/toggle/gen1_status":
+        if msg == b"True":
+            relay_gen1.value(0)
+        elif msg == b"False":
+            relay_gen1.value(1)
+    elif topic == "switch/toggle/gen2_status":
+        if msg == b"True":
+            relay_gen2.value(0)
+        elif msg == b"False":
+            relay_gen2.value(1)
+    elif topic == "switch/toggle/gen3_status":
+        if msg == b"True":
+            relay_gen3.value(0)
+        elif msg == b"False":
+            relay_gen3.value(1)
+    elif topic == "switch/toggle/gen4_status":
+        if msg == b"True":
+            relay_gen4.value(0)
+        elif msg == b"False":
+            relay_gen4.value(1)
     else:
         print("Unknown topic")
     mlha.publish_status(parse_message())
@@ -125,6 +161,10 @@ def parse_message():
                       "acs_status": relay_acs.value() == 0,
                       "primerod_status": relay_primerod.value() == 0,
                       "garaje_status": relay_garaje.value() == 0,
+                      "gen1_status": relay_gen1.value() == 0,
+                      "gen2_status": relay_gen2.value() == 0,
+                      "gen3_status": relay_gen3.value() == 0,
+                      "gen4_status": relay_gen4.value() == 0,
                       "mltemp_connection": True}
 
     return extracted_data
@@ -136,16 +176,20 @@ def read_and_publish():
 
 # Publishes the config for the sensors to Homeassistant
 def setup_config():
-    mlha.publish_config("caldera_temp", "Temperatura de la Caldera", "sensor", "temperature", "C", "measurement", expire_after = 60)
-    mlha.publish_config("casa_temp", "Temperatura de Casa", "sensor", "temperature", "C", "measurement", expire_after = 60)
-    mlha.publish_config("exterior_temp", "Temperatura Exterior", "sensor", "temperature", "C", "measurement", expire_after = 60)
-    mlha.publish_config("deposito_temp", "Temperatura del Deposito", "sensor", "temperature", "C", "measurement", expire_after = 60)
+    mlha.publish_config("caldera_temp", "Temperatura de la Caldera", "sensor", "temperature", None, "measurement", expire_after = 60)
+    mlha.publish_config("casa_temp", "Temperatura de Casa", "sensor", "temperature", None, "measurement", expire_after = 60)
+    mlha.publish_config("exterior_temp", "Temperatura Exterior", "sensor", "temperature", None, "measurement", expire_after = 60)
+    mlha.publish_config("deposito_temp", "Temperatura del Deposito", "sensor", "temperature", None, "measurement", expire_after = 60)
     mlha.publish_config("caldera_status", "Estado de la caldera", "switch", expire_after = 60)
     mlha.publish_config("acs_status", "Estado del ACS", "switch", expire_after = 60)
     mlha.publish_config("primerod_status", "Estado del Primero D", "switch", expire_after = 60)
     mlha.publish_config("garaje_status", "Estado del Garaje", "switch", expire_after = 60)
     mlha.publish_config("cuadra_motion", "Movimiento en la cuadra", "binary_sensor", "motion", state_topic = "/motion", expire_after = 60)
     mlha.publish_config("mltemp_connection", "MLTemp Connection", "binary_sensor", "connectivity", expire_after = 60)
+    mlha.publish_config("gen1_status", "Estado del rele gen 1", "switch", expire_after = 60)
+    mlha.publish_config("gen2_status", "Estado del rele gen 2", "switch", expire_after = 60)
+    mlha.publish_config("gen3_status", "Estado del rele gen 3", "switch", expire_after = 60)
+    mlha.publish_config("gen4_status", "Estado del rele gen 4", "switch", expire_after = 60)
 
 # Main =============================================
 # Initialize main component (WiFi, MQTT and HomeAssistant)
@@ -166,10 +210,18 @@ relay_caldera = Pin(relay_caldera_pin, Pin.OUT)
 relay_acs = Pin(relay_acs_pin, Pin.OUT)
 relay_primerod = Pin(relay_primerod_pin, Pin.OUT)
 relay_garaje = Pin(relay_garaje_pin, Pin.OUT)
+relay_gen1 = Pin(relay_gen1_pin, Pin.OUT)
+relay_gen2 = Pin(relay_gen2_pin, Pin.OUT)
+relay_gen3 = Pin(relay_gen3_pin, Pin.OUT)
+relay_gen4 = Pin(relay_gen4_pin, Pin.OUT)
 relay_caldera.value(1)
 relay_acs.value(1)
 relay_primerod.value(1)
 relay_garaje.value(1)
+relay_gen1.value(1)
+relay_gen2.value(1)
+relay_gen3.value(1)
+relay_gen4.value(1)
 
 # Initialize PIR sensor
 print("Initializing PIR sensor")
@@ -181,6 +233,10 @@ mlha.subscribe("switch/toggle/caldera_status")
 mlha.subscribe("switch/toggle/acs_status")
 mlha.subscribe("switch/toggle/primerod_status")
 mlha.subscribe("switch/toggle/garaje_status")
+mlha.subscribe("switch/toggle/gen1_status")
+mlha.subscribe("switch/toggle/gen2_status")
+mlha.subscribe("switch/toggle/gen3_status")
+mlha.subscribe("switch/toggle/gen4_status")
 print("Connected to MQTT broker and subscribed to topics")
 
 # Publish config for sensors
@@ -207,11 +263,12 @@ while True:
             else:
                 print("Motion stopped")
                 mlha.publish("motion/state", "False")
-        # Send data to broker every 30 seconds
-        if time.ticks_diff(time.ticks_ms(), last_update) > 30000: # 30 seconds
+        # Send data to broker every 15 seconds
+        if time.ticks_diff(time.ticks_ms(), last_update) > 15000: # 15 seconds
             last_update = time.ticks_ms()
             read_and_publish()
         time.sleep_ms(250)
     except Exception as ex:
         print("error: " + str(ex))
+        time.sleep_ms(250)
         machine.reset()
